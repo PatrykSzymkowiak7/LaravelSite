ca98c9340fab264e15561abd3df32a53a2e15d90
PatrykSzymkowiak7 <patrykszymkowiak7@gmail.com>
2021-10-15 21:01:13 +0200

    routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,59 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cdv', function () {
+    return view('cdv');
+});
+
+Route::get('/cdv', function () {
+    return ['name' => 'cdv', 'base' => 'classic'];
+});
+
+Route::get('/cdv', function () {
+    return view('cdv', ['name' => 'Patryk', 'surname' => 'Szymkowiak', 'city' => 'Poznań']);
+});
+
+Route::get('/pages/{x}', function($x){
+    $pages = [
+        'about' => 'Strona CDV',
+        'contact' => 'Poznań; ul. Polna 13',
+        'home' => 'Strona domowa'
+    ];
+    return $pages[$x];
+});
+
+Route::get('/address/{city?}/{street?}/{zipcode?}', function(String $city = "brak danych", 
+String $street = " - ", int $zipcode = null){
+    if(is_null($zipcode))
+        $zipcode = "brak";
+    else
+        $zipcode = substr($zipcode,0,2)."-".substr($zipcode,2,3);
+    echo <<<ADDRESS
+    Kod pocztowy: $zipcode<br>
+    Miasto: $city<br>
+    Ulica: $street
+    <hr>
+ADDRESS;
+})->name('address');
+
+Route::redirect('/adres/{city?}/{street?}/{zipcode?}', '/address/{city?}/{street?}/{zipcode?}');
+
+Route::prefix('admin')->group(function(){
+    Route::get('/home/{name}', function(String $name){
+        echo "Witaj $name na stronie administracyjnej";
+    });
+
+    Route::get('users', function(){
+        echo "<h3>Użytkownicy systemu</h3>";
+    });
+});
+
+Route::redirect('/admin/{name}', '/admin/home/{name}');
+
+Route::get('/user/{name}/{age?}', function(String $name, int $age = null){
+    echo "Imię: $name";
+    if($age != null){
+        echo "<br>Wiek: $age";
+    }
+})->where(['name' => '[A-Zz-z]+', 'age' => '[0-9]+']);
