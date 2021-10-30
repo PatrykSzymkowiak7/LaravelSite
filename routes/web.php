99d317abb1fd292b43bd758a70c46bb7ebe172f5
PatrykSzymkowiak7 <patrykszymkowiak7@gmail.com>
2021-10-30 18:23:57 +0200

    Some changes on routes and controllers
    Some changes on routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -17,58 +17,46 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cdv', function () {
-    return view('cdv');
-});
-
-Route::get('/cdv', function () {
-    return ['name' => 'cdv', 'base' => 'classic'];
-});
-
-Route::get('/cdv', function () {
-    return view('cdv', ['name' => 'Patryk', 'surname' => 'Szymkowiak', 'city' => 'Poznań']);
+Route::get('/wsb', function (){
+  // echo 'WSB';
+  // return view('wsb');
+  // return ['name' => 'Janusz', 'surname' => 'Nowak'];
+  return view('wsb', ['name' => 'Patryk', 'surname' => 'Szymkowiak']);
 });
 
 Route::get('/pages/{x}', function($x){
-    $pages = [
-        'about' => 'Strona CDV',
-        'contact' => 'Poznań; ul. Polna 13',
-        'home' => 'Strona domowa'
-    ];
-    return $pages[$x];
+  $pages = [
+    'about' => 'Strona WSB',
+    'contact' => 'anna@o2.pl',
+    'home' => 'Strona domowa'
+  ];
+  return $pages[$x];
 });
 
-Route::get('/address/{city?}/{street?}/{zipcode?}', function(String $city = "brak danych", 
-String $street = " - ", int $zipcode = null){
-    if(is_null($zipcode))
-        $zipcode = "brak";
-    else
-        $zipcode = substr($zipcode,0,2)."-".substr($zipcode,2,3);
-    echo <<<ADDRESS
-    Kod pocztowy: $zipcode<br>
-    Miasto: $city<br>
+Route::get('/address/{city?}/{street?}/{zipCode?}', function(String $city = '-', String $street = '-', int $zipCode = null){
+  $zipCode = substr($zipCode, 0, 2)."-".substr($zipCode, 2, 3);
+  echo <<<ADDRESS
+    Kod pocztowy: $zipCode,
+     miasto: $city<br>
     Ulica: $street
     <hr>
 ADDRESS;
 })->name('address');
 
-Route::redirect('/adres/{city?}/{street?}/{zipcode?}', '/address/{city?}/{street?}/{zipcode?}');
+Route::redirect('adres/{city?}/{street?}/{zipCode?}', '/address/{city?}/{street?}/{zipCode?}');
 
 Route::prefix('admin')->group(function(){
-    Route::get('/home/{name}', function(String $name){
-        echo "Witaj $name na stronie administracyjnej";
-    });
+  Route::get('/home/{name}', function(String $name){
+    echo "Witaj $name na stronie";
+  })->where(['name' => '[A-Za-z]+']);
 
-    Route::get('users', function(){
-        echo "<h3>Użytkownicy systemu</h3>";
-    });
+  Route::get('/users', function(){
+    echo "Użytkownicy serwisu";
+  });
 });
 
 Route::redirect('/admin/{name}', '/admin/home/{name}');
 
-Route::get('/user/{name}/{age?}', function(String $name, int $age = null){
-    echo "Imię: $name";
-    if($age != null){
-        echo "<br>Wiek: $age";
-    }
-})->where(['name' => '[A-Zz-z]+', 'age' => '[0-9]+']);
+Route::get("/site/{CdvSite}", "CdvSite@index");
+
+Route::get('site/{wsbsite}', [WsbSite::class, 'index']);
